77e29bddc1279fbaa4a4ade5089a81e53cd2d900
Bino26 <hbino1999@gmail.com>
2023-10-03 02:30:37 +0000

    Answers for lesson 13

--- app.js
+++ app.js
@@ -1,46 +1,181 @@
 const express = require("express");
-const path = require("path");
 const session = require("express-session");
+require("dotenv").config();
 const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
 const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
+const bcrypt = require("bcryptjs");
+const MongoDBStore = require("connect-mongodb-session")(session);
+const cookieParser = require("cookie-parser");
+const csrf = require("csurf");
 
-const mongoDb = "YOUR MONGO URL HERE";
+let secretString = "Beginning value";
+
+var store = new MongoDBStore({
+  uri: process.env.MONGO_URI,
+  collection: "sessions",
+});
+
+// Catch errors
+store.on("error", function (error) {
+  console.log(error);
+});
+
+const mongoDb = process.env.MONGO_URI;
 mongoose.connect(mongoDb, { useUnifiedTopology: true, useNewUrlParser: true });
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "mongo connection error"));
 
 const User = mongoose.model(
   "User",
-  new Schema({
+  new mongoose.Schema({
     username: { type: String, required: true },
     password: { type: String, required: true },
   })
 );
 
+const authMiddleware = (req, res, next) => {
+  if (!req.user) {
+    if (!req.session.messages) {
+      req.session.messages = [];
+    }
+    req.session.messages.push("You can't access that page before logon.");
+    res.redirect("/");
+  } else {
+    next();
+  }
+};
+
 const app = express();
 app.set("views", __dirname);
 app.set("view engine", "ejs");
 
-app.use(session({ secret: "cats", resave: false, saveUninitialized: true }));
+app.use(
+  //session
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    store: store,
+  })
+);
 app.use(passport.initialize());
 app.use(passport.session());
-app.use(express.urlencoded({ extended: false }));
+app.use((req, res, next) => {
+  console.log("req.user:", req.user);
+  res.locals.currentUser = req.user;
+  next();
+});
 
-app.get("/", (req, res) => res.render("index"));
-app.get("/sign-up", (req, res) => res.render("sign-up-form"));
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser(process.env.SESSION_SECRET));
+const csrfProtection = csrf({ cookie: true });
+app.use(csrfProtection);
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
+});
+
+passport.use(
+  new LocalStrategy(async (username, password, done) => {
+    try {
+      const user = await User.findOne({ username: username });
+      if (!user) {
+        return done(null, false, { message: "Incorrect username" });
+      }
+      bcrypt.compare(password, user.password, (err, result) => {
+        if (result) {
+          return done(null, user);
+        } else {
+          return done(null, false, { message: "Incorrect password" });
+        }
+      });
+    } catch (err) {
+      return done(err);
+    }
+  })
+);
+
+passport.authenticate("local", {
+  successRedirect: "/",
+  failureRedirect: "/",
+  failureMessage: true,
+});
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await User.findById(id);
+    done(null, user);
+  } catch (err) {
+    done(err);
+  }
+});
+
+app.get("/", (req, res) => {
+  let messages = [];
+  if (req.session.messages) {
+    messages = req.session.messages;
+    req.session.messages = [];
+  }
+  res.render("index", { messages, csrfToken: req.csrfToken() });
+});
+
+app.get("/sign-up", (req, res) => {
+  res.render("sign-up-form", { csrfToken: req.csrfToken() });
+});
+
 app.post("/sign-up", async (req, res, next) => {
   try {
-    const user = new User({
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    await User.create({
       username: req.body.username,
-      password: req.body.password,
+      password: hashedPassword,
     });
-    const result = await user.save();
     res.redirect("/");
   } catch (err) {
     return next(err);
   }
+});
+
+app.post(
+  "/log-in",
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/",
+    failureMessage: true,
+  })
+);
+
+app.get("/restricted", authMiddleware, (req, res) => {
+  if (!req.session.pageCount) {
+    req.session.pageCount = 1;
+  } else {
+    req.session.pageCount++;
+  }
+  res.render("restricted", {
+    pageCount: req.session.pageCount,
+    secretString,
+    csrfToken: req.csrfToken(),
+  });
+});
+
+app.post("/restricted", authMiddleware, (req, res) => {
+  secretString = req.body.secretString;
+  res.redirect("/restricted");
+});
+
+app.get("/log-out", (req, res) => {
+  req.session.destroy((err) => {
+    if (err) {
+      console.log(err);
+    }
+    res.redirect("/");
+  });
 });
 
 app.listen(3000, () => console.log("app listening on port 3000!"));
